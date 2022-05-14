702274bb07464a36568affd55bffa3071986a733
daejah-atkinson <daejah.atkinson@gmail.com>
2022-05-14 17:32:19 -0400

    frontend created, working on header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = ()=> {
+    return(
+        <div className='header'>
+            <div className='header-left'>
+                <h1>Pack Hacker</h1>
+                <h3>Never pack from wrong clothes again!</h3>
+            </div>
+            <div className='header-right'>
+                <button>Log In</button>
+                <button>Sign Up</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+const Main = () => {
+    return(
+        <h1>Main Component</h1>
+    )
+}
+
+export default Main;
