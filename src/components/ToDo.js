cb6f70f3892545c3340685aeac0658347ef7279c
daejah-atkinson <daejah.atkinson@gmail.com>
2022-05-17 08:40:23 -0400

    todo list created

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -8,8 +8,31 @@ const ToDo = (props)=>{
     let days = difference / (1000 * 3600 * 24)
     console.log(days)
     return(
-        <div>
-            <h1>To Do</h1>
+        <div className='listitem-container'>
+            <div className='list-header'>
+            <h1 >To Do</h1>
+            <p className='todo-p'>Things to do before your {days} day trip.</p>
+            </div>
+            <div className='listitem'>
+                <input type='checkbox' id='item'/>
+                <label for='item'>Water the plants</label>
+            </div>
+            <div className='listitem'>
+                <input type='checkbox' id='item'/>
+                <label for='item'>Clean out the fridge</label>
+            </div>
+            <div className='listitem'>
+                <input type='checkbox' id='item'/>
+                <label for='item'>Charge electronic devices</label>
+            </div>
+            <div className='listitem'>
+                <input type='checkbox' id='item'/>
+                <label for='item'>Pause the mail</label>
+            </div>
+            <div className='listitem'>
+                <input type='checkbox' id='item'/>
+                <label for='item'>Online Check-in</label>
+            </div>
         </div>
     )
 }
